f378e12eed04b27c5cbb62e81f2b1c74f7eddba3
James Watson <jimwatson@mac.com>
2019-03-25 07:01:14 +0300

    Extract the probability of prediction.  we still need to compare the E and F2 probabilities and extend the Path struct to store the final value.

--- P533/Src/P533/MUFVariability.c
+++ P533/Src/P533/MUFVariability.c
@@ -10,17 +10,17 @@
 void MUFVariability(struct PathData *path) {
 	/*
 
-	 	MUFVariability() - Calculates the F2 and E Fprob, 50% MUF (MUF50), 90% MUF (MUF90) and 10% MUF (MUF10) 
+	 	MUFVariability() - Calculates the F2 and E Fprob, 50% MUF (MUF50), 90% MUF (MUF90) and 10% MUF (MUF10)
 	 		in accordance with P.533-12
 	 		Section 3.7 "The path operational MUF" and
-	 		Section 3.6 "Within the month probability of ionospheric propagation support" 
-	 
+	 		Section 3.6 "Within the month probability of ionospheric propagation support"
+
 	 		INPUT
 	 			struct PathData *path
-	 
+
 	 		OUTPUT
-	 			path->MUF50 = Path basic MUF 50% of a month 
-	 			path->MUF90 = Path basic MUF 90% of a month 
+	 			path->MUF50 = Path basic MUF 50% of a month
+	 			path->MUF90 = Path basic MUF 90% of a month
 	 			path->Md_E[].MUF50 = E layer 50% of the month MUF
 	 			path->Md_F2[].MUF50 = F2 Layer 50% of the month MUF
 	 			path->Md_E[].MUF90 = E layer 90% of the month MUF
@@ -29,9 +29,9 @@ void MUFVariability(struct PathData *path) {
 	 			path->Md_F2[].deltau = F2 layer upper decile deviation of the MUF
 	 			path->Md_E[].deltal = E layer lower decile deviation of the MUF
 	 			path->Md_E[].deltau = E layer lower decile deviation of the MUF
-	 			path->Md_F2[].Fprob = F2 layer within the month probability of ionospheric propagation support 
+	 			path->Md_F2[].Fprob = F2 layer within the month probability of ionospheric propagation support
 	 			path->Md_E[].Fprob = E layer within the month probability of ionospheric propagation support
-	 	
+
 
 			SUBROUTINES
 				FindfoF2var()
@@ -41,20 +41,23 @@ void MUFVariability(struct PathData *path) {
 	int i;		// Index
 	int decile; // Decile flag
 
-	double EMUF10, EMUF90;		// Temp 10% and 90% E layer MUF
-	double F2MUF10, F2MUF90;	// Temp 10% and 90% F2 layer MUF
+	double EMUF10, EMUF90, EProb;		// Temp 10% and 90% E layer MUF, EProb
+	double F2MUF10, F2MUF90, F2Prob;	// Temp 10% and 90% F2 layer MUF, F2Prob
+	double bbProb;
+	int testFlag;
 
-	
+
 	// Only do this subroutine if the path is less than or equal to 9000 km if not exit
-	if(path->distance > 9000) return; 
-	
+	if(path->distance > 9000) return;
+
 	// Section 3.65 seems to imply that the basic MUF for the path and the 50% MUF are the same so for this calculation set them equal
 	path->MUF50 = path->BMUF;
 
 	// There potentially are 3 E layer modes and 6 F2 layer modes
 
 	// The F2 layer modes
-	for(i=0; i<MAXF2MDS; i++) { // There are 6 F2 layer modes 
+	printf("\n");
+	for(i=0; i<MAXF2MDS; i++) { // There are 6 F2 layer modes
 		if(path->Md_F2[i].BMUF != 0.0) { // If the Basic MUF is set, non-zero, the layer exists
 
 			// Section 3.6 P.533-12 indicates that the basic MUF and MUF(50) are the same.
@@ -64,7 +67,7 @@ void MUFVariability(struct PathData *path) {
 			decile = DL; // Lower decile
 			// Find the deltal in the foF2var array
 			path->Md_F2[i].deltal = FindfoF2var(*path, path->CP[MP].ltime, path->CP[MP].L.lat, decile);
-				
+
 			decile = DU; // Upper decile
 			// Find the deltau in the foF2var array
 			path->Md_F2[i].deltau = FindfoF2var(*path, path->CP[MP].ltime, path->CP[MP].L.lat, decile);
@@ -76,26 +79,29 @@ void MUFVariability(struct PathData *path) {
 			// Now determine the probability that the mode can be supported
 			if(path->frequency < path->Md_F2[i].MUF50) {
 				path->Md_F2[i].Fprob = min((130 - (80.0/(1.0 + (path->Md_F2[i].MUF50/(path->frequency*path->Md_F2[i].deltal))))), 100.0);
+				bbProb = min(130.0 - (80.0 / (1.0 + ((1.0-(path->frequency/path->Md_F2[i].MUF50))/(1.0-path->Md_F2[i].deltal)))), 100.0);
 			}
 			else { // (path->frequency >= path->Md_F2[i].MUF50)
 				path->Md_F2[i].Fprob = max(((80.0/(1.0 + (path->frequency/(path->Md_F2[i].MUF50*path->Md_F2[i].deltau)))) - 30.0), 0.0);
+				bbProb = max((80.0 / (1.0 + (((path->frequency/path->Md_F2[i].MUF50)-1.0) / (path->Md_F2[i].deltau - 1.0)))) - 30.0, 0.0);
 			}
+			printf("(%d) Freq: %.1fMHz MUF90: %.2f MUF10: %.2f LowerDec: %.1f UpperDec: %.1f P533 Prob: %.2f BBProb: %.2f\n", i, path->frequency, path->Md_F2[i].MUF90, path->Md_F2[i].MUF10, path->Md_F2[i].deltal, path->Md_F2[i].deltau, path->Md_F2[i].Fprob, bbProb);
 		}
 	}
 
 	// The following loop is the same as that for the F2 layer but the decile values are fixed according to Section 3.6
 	// The lower decile delta is 0.95 and the upper is 1.05/
 	// Find the E layer MUFs
-	for(i=0; i<MAXEMDS; i++) { // There are 6 F2 layer modes 
+	for(i=0; i<MAXEMDS; i++) { // There are 6 F2 layer modes
 		if(path->Md_E[i].BMUF != 0.0) { // If the Basic MUF is set, non-zero, the layer exists
 
 			// Section 3.6 P.533-12 indicates that the basic MUF and MUF(50) are the same.
 			path->Md_E[i].MUF50 = path->Md_E[i].BMUF;
-			
+
 			// deltal for E layer
 			path->Md_E[i].deltal = 0.95;
 
-			// deltau for the E layer 
+			// deltau for the E layer
 			path->Md_E[i].deltau = 1.05;
 
 			// Find the other MUFs
@@ -117,18 +123,46 @@ void MUFVariability(struct PathData *path) {
 	// Pick the MUF extrema for the path for the E and F2 layer
 	EMUF10 = 0.0;
 	EMUF90 = 0.0;
+	EProb = 0.0;
 	for(i=0; i<MAXEMDS; i++) {
 		if(path->Md_E[i].BMUF != 0.0) { // If the Basic MUF is set the layer exists
-			if(path->Md_E[i].MUF90 > EMUF90) EMUF90 = path->Md_E[i].MUF90;
-			if(path->Md_E[i].MUF10 > EMUF10) EMUF10 = path->Md_E[i].MUF10;
+			if(path->Md_E[i].MUF90 > EMUF90) {
+				EMUF90 = path->Md_E[i].MUF90;
+				if (path->frequency < path->Md_E[i].MUF50) {
+					EProb = path->Md_E[i].Fprob;
+				}
+			}
+			if(path->Md_E[i].MUF10 > EMUF10) {
+				EMUF10 = path->Md_E[i].MUF10;
+				if (path->frequency >= path->Md_E[i].MUF50) {
+					EProb = path->Md_E[i].Fprob;
+				}
+			}
 		};
 	};
+
 	F2MUF10 = 0.0;
 	F2MUF90 = 0.0;
+	F2Prob = 0.0;
 	for(i=0; i<MAXF2MDS; i++) {
 		if(path->Md_F2[i].BMUF != 0.0) { // If the Basic MUF is set the layer exists
-			if(path->Md_F2[i].MUF90 > F2MUF90) F2MUF90 = path->Md_F2[i].MUF90;
-			if(path->Md_F2[i].MUF10 > F2MUF10) F2MUF10 = path->Md_F2[i].MUF10;
+			testFlag = 0;
+			if(path->Md_F2[i].MUF90 > F2MUF90) {
+				F2MUF90 = path->Md_F2[i].MUF90;
+				if (path->frequency < path->Md_F2[i].MUF50) {
+					F2Prob = path->Md_F2[i].Fprob;
+				}
+				testFlag = 1;
+			}
+			if(path->Md_F2[i].MUF10 > F2MUF10) {
+				F2MUF10 = path->Md_F2[i].MUF10;
+				if (path->frequency >= path->Md_F2[i].MUF50) {
+					F2Prob = path->Md_F2[i].Fprob;
+				}
+				if (testFlag == 0) {
+					printf("*** We have a strange MUF ***\n");
+				}
+			}
 		};
 	};
 
@@ -136,29 +170,31 @@ void MUFVariability(struct PathData *path) {
 	path->MUF90 = max(EMUF90, F2MUF90); // largest 90% MUF
 	path->MUF10 = max(EMUF10, F2MUF10); // largest 10% MUF
 
+	printf("MUF90: %.2f MUF10: %.2f EProb: %.2f F2Prob: %.2f\n", path->MUF90, path->MUF10, EProb, F2Prob);
+
 	return;
 
 };
 
-double FindfoF2var(struct PathData path, double hour, double lat, int decile) { 
+double FindfoF2var(struct PathData path, double hour, double lat, int decile) {
 
 	/*
 
 	  FindfoF2var() - Determines the variation in foF2 given the season, hour, latitude ss and decile
 	 		This routine uses the bilinear interpolation method in ITU-R P.1144-5
-	 
+
 	 		INPUT
-	 			struct PathData path 
+	 			struct PathData path
 	 			double hour - Hour of interest
 	 			double lat - Latitude of interest
 	 			int decile - Upper or lower decile index
-	 
+
 	 		OUTPUT
 	 			return interpolated value
 
 			SUBROUTINES
 				BilinearInterpolation()
-	 
+
 	 */
 
 	double UR, UL, LL, LR; // Neighboring values
@@ -181,14 +217,14 @@ double FindfoF2var(struct PathData path, double hour, double lat, int decile) {
 
 	latL = (int)floor(lat);
 	latU = (int)ceil(lat);
-	
+
 	if(latL < 0) {
 		latL = 18; // rollunder
 		// The sense of the fractional row is reversed when negative so fix it
 		r = 1.0 - r;
 	};
 	if(latU > 18) {
-		latU = 0; // rollover 
+		latU = 0; // rollover
 	};
 
 	hourL = (int)floor(hour);
@@ -199,17 +235,17 @@ double FindfoF2var(struct PathData path, double hour, double lat, int decile) {
 		// The sense of the fractional column is reversed when negative so fix it
 		c = 1.0 - c;
 	};
-	
+
 	if(hourU > 23) {
 		hourU = 0; // rollover hour
 	};
 
-	// Determine the sunspot number index ssn. 
+	// Determine the sunspot number index ssn.
 	if(path.SSN < 50) {
 		ssn = 0;
 	}
-	else if((50 <= path.SSN) && (path.SSN <=100)) { 
-		ssn = 1;	
+	else if((50 <= path.SSN) && (path.SSN <=100)) {
+		ssn = 1;
 	}
 	else { // path-SSN > 100
 		ssn = 2;
